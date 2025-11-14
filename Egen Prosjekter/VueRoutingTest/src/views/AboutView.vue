09375561aad194b1d447cbab8c09e5ac814b67ec
SanderK36 <sander.k606@gmail.com>
2025-11-14 23:04:20 +0100

    14.11.25

---
+++ Egen Prosjekter/VueRoutingTest/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Hei fra side 2</h1>
+    <h1 class="hh">Jeg elsker deg ❤️</h1>
+</template>
+
+<style scoped>
+    h1 {
+        font-size: 50px;
+        color: red;
+        font-family: Arial, Helvetica, sans-serif;
+        margin-left: 30%;
+    }
+
+    .hh {
+        margin-left: 27%;
+    }
+</style>
---
+++ Egen Prosjekter/VueRoutingTest/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Hei fra side 1</h1>
+</template>
+
+<style scoped>
+    h1 {
+        font-size: 50px;
+        color: blue;
+        font-family: Arial, Helvetica, sans-serif;
+        margin-left: 30%;
+    }
+</style>
